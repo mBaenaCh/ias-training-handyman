13fb6f368696237236cefcee8f8fe2f4131bb053
mBaenaCh <mateo.baena@hotmail.com>
2022-01-31 10:51:28 -0500

    first commit

---
+++ src/test/java/com/example/handyman/HandymanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.handyman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandymanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
