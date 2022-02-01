c8c816619f7913350d81067621bc15d9980aaa53
mBaenaCh <mateo.baena@hotmail.com>
2022-02-01 13:01:39 -0500

    Tests: Service domain class

---
+++ src/test/java/com/example/handyman/calculator/domain/ServiceTest.java
@@ -0,0 +1,40 @@
+package com.example.handyman.calculator.domain;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+class ServiceTest {
+
+    Id serviceId = Id.generateUUID();
+    ServiceType type = ServiceType.Normal;
+
+    @Test
+    public void shouldReturnNullPointerExceptionIfTheServiceIdIsNull(){
+        //Arrange
+        serviceId = null;
+
+        //Act
+        Executable executable = () -> new Service(serviceId, type);
+
+        //Assert
+        assertThrows(NullPointerException.class, executable);
+    }
+
+    @Test
+    public void shouldReturnNullPointerExceptionIfTheTypeIsNull(){
+        //Arrange
+        type = null;
+
+        //Act
+        Executable executable = () -> new Service(serviceId, type);
+
+        //Assert
+        assertThrows(NullPointerException.class, executable);
+
+    }
+}
