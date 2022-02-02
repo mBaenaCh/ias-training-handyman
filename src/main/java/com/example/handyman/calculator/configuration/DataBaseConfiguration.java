195640e6cf673f292b05bed0e2bcd0fe9d696b57
mBaenaCh <mateo.baena@hotmail.com>
2022-02-01 21:19:00 -0500

    Config: Database connection

---
+++ src/main/java/com/example/handyman/calculator/configuration/DataBaseConfiguration.java
@@ -0,0 +1,30 @@
+package com.example.handyman.calculator.configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataBaseConfiguration {
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:postgresql://localhost:5432/handyman");
+        config.setUsername("postgres");
+        config.setPassword("postgres");
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(
+            DataSource dataSource
+    ) {
+        return new JdbcTemplate(dataSource);
+    }
+
+}
