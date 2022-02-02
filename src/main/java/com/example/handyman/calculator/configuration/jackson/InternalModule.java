c7a1ab352970cf4c6484432f363b3301a0ce55b4
mBaenaCh <mateo.baena@hotmail.com>
2022-02-02 12:49:19 -0500

    Config: Adding missing serializers and deserializers

--- src/main/java/com/example/handyman/calculator/configuration/jackson/InternalModule.java
+++ src/main/java/com/example/handyman/calculator/configuration/jackson/InternalModule.java
@@ -1,7 +1,11 @@
 package com.example.handyman.calculator.configuration.jackson;
 
 import com.example.handyman.calculator.configuration.jackson.codecs.IdParser;
+import com.example.handyman.calculator.configuration.jackson.codecs.LastNameParser;
+import com.example.handyman.calculator.configuration.jackson.codecs.NameParser;
 import com.example.handyman.calculator.domain.Id;
+import com.example.handyman.calculator.domain.LastName;
+import com.example.handyman.calculator.domain.Name;
 import com.fasterxml.jackson.core.Version;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
@@ -14,7 +18,11 @@ public class InternalModule extends SimpleModule {
         addSerializer(Id.class, new IdParser.Serializer());
         addDeserializer(Id.class, new IdParser.Deserializer());
 
+        addSerializer(Name.class, new NameParser.Serializer());
+        addDeserializer(Name.class, new NameParser.Deserializer());
 
+        addSerializer(LastName.class, new LastNameParser.Serializer());
+        addDeserializer(LastName.class, new LastNameParser.Deserializer());
 
     }
 }
