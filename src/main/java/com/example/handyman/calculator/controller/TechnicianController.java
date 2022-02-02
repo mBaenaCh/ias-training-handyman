a124cd629d9288d099088948ad0b07417da16082
mBaenaCh <mateo.baena@hotmail.com>
2022-02-02 10:54:45 -0500

    Feature: technician controller

---
+++ src/main/java/com/example/handyman/calculator/controller/TechnicianController.java
@@ -0,0 +1,89 @@
+package com.example.handyman.calculator.controller;
+
+import com.example.handyman.calculator.domain.Id;
+import com.example.handyman.calculator.domain.Technician;
+import com.example.handyman.calculator.service.TechnicianService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value="/api/technicians")
+public class TechnicianController {
+
+    private TechnicianService service;
+
+    public TechnicianController(TechnicianService service){
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Technician> create(
+            @RequestBody Technician technician){
+
+        return new ResponseEntity<>(
+                service.create(technician),
+                HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Technician>> getAll(){
+        return new ResponseEntity<>(
+                service.getAll(),
+                HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Technician> getById(
+            @PathVariable("id") String id){
+
+        Id technicianId = Id.generateUUIDFromString(id);
+        Technician foundTechnician = service.getById(technicianId);
+        if(foundTechnician != null){
+            return new ResponseEntity<>(
+                    foundTechnician,
+                    HttpStatus.OK);
+        }else{
+            return new ResponseEntity(
+                    "The technician with the given ID was not found",
+                    HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Technician> updateById(
+            @PathVariable("id") String id,
+            @RequestBody Technician technician){
+
+        Id technicianId = Id.generateUUIDFromString(id);
+
+        if(service.updateById(technicianId, technician)){
+            return new ResponseEntity<>(
+                    technician,
+                    HttpStatus.OK);
+        }else{
+            return new ResponseEntity(
+                    "The technician with the given ID was not found",
+                    HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity deleteById(
+            @PathVariable("id") String id){
+
+        Id technicianId = Id.generateUUIDFromString(id);
+
+        if(service.deleteById(technicianId)){
+            return  new ResponseEntity(
+                    "Deleted succesfuly",
+                    HttpStatus.OK);
+        }else{
+            return new ResponseEntity(
+                    "The technician with the given ID was not found",
+                    HttpStatus.NOT_FOUND);
+        }
+    }
+}
--- src/main/java/com/example/handyman/calculator/service/TechnicianService.java
+++ src/main/java/com/example/handyman/calculator/service/TechnicianService.java
@@ -47,6 +47,5 @@ public class TechnicianService {
         }else {
             return false;
         }
-
     }
 }
